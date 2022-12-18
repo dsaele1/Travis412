9eddada32eead209bbaeebe402a990dab33ed17b
drsae <drsae@DESKTOP-ND4DFL6>
2022-12-17 21:45:34 -0500

    pushed test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,17 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	public void testSub() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.sub(1, 1), 0);
+	}
+	public void testMult() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mult(1, 5), 5);
+	}
+	public void testDiv() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.div(8, 2), 4);
+	}
 }
+	
