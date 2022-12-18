89a49117a413016c16673cf0c8e95bc6e4cc7097
drsae <drsae@DESKTOP-ND4DFL6>
2022-12-17 21:27:49 -0500

    PUSHED GOOD FILES

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,13 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int mult(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
 }
